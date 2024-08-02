4d40e6b5c6395d7cd8842b5133258ee02f9610b6
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-08-02 16:11:12 -0300

    alterado

--- mais.js
+++ mais.js
@@ -11,4 +11,22 @@ Meu_soma.get('/calculo/somar/:n1/:n2', ( req, resp ) => {
     resp.send(`A soma é de ${soma}`);
 })
 
-Meu_soma.listen(3500, () => console.log('API Subiu'))
+Meu_soma.listen(3500, () => console.log('Mamãe dormiu, API Subiu'))
+
+Meu_soma.get('/calculo/subtrair/:n1/:n2', ( req, resp ) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let subtrair = ( n1 - n2 );
+
+    resp.send(`A subtração é de ${subtrair}`);
+})
+
+Meu_soma.get('/calculo/multiplicação/:n1/:n2', ( req, resp ) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let mult = ( n1 * n2 );
+
+    resp.send(`A multiplicação é de ${mult}`);
+})
